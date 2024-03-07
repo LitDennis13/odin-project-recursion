4a097a7e5d013a87c6b65905b578d68b2cb11067
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-03-07 16:49:45 -0600

    created recursive merge sort algorithm

--- script.js
+++ script.js
@@ -23,16 +23,64 @@ function recursiveFib(arrLen) {
     }
 }
 
-// \\ [0,1,1,2,3,5,8,13,21,34] 10
-// // [0,1,1,2,3,5,8,13,21] 9
-// \\ [0,1,1,2,3,5,8,13] 8
-// // [0,1,1,2,3,5,8] 7
-// \\ [0,1,1,2,3,5] 6
-// // [0,1,1,2,3] 5
-// \\ [0,1,1,2] 4
-// // [0,1,1] 3
-// \\ [0,1] 2
-// // [0] 1
+function mergeSort(arr) {
+    if (arr.length > 1) {
+        let middleArrNum = Math.floor(arr.length/2);
+        let left = mergeSort(arr.slice(0,middleArrNum));
+        let right = mergeSort(arr.slice(middleArrNum,arr.length));
 
+        let sorted = false;
+        let leftIndex = 0;
+        let rightIndex = 0;
+        let  mergedArray = [];
+        let mergedArrayIndex = 0;
 
+        while (!sorted) {
+            if (leftIndex >= left.length && rightIndex >= right.length) {
+                sorted = true;
+                break;
+            }
+            else if (rightIndex !== right.length && leftIndex === left.length) {
+                mergedArray[mergedArrayIndex] = right[rightIndex];
+                mergedArrayIndex++;
+                rightIndex++;
+                continue;
+            }
+            else if (leftIndex !== left.length && rightIndex === right.length) {
+                mergedArray[mergedArrayIndex] = left[leftIndex];
+                mergedArrayIndex++;
+                leftIndex++;
+                continue;
+            }
+            else if (left[leftIndex] < right[rightIndex]) {
+                mergedArray[mergedArrayIndex] = left[leftIndex];
+                mergedArrayIndex++;
+                leftIndex++;
+                continue;
+            }
+            else if (right[rightIndex] < left[leftIndex]) {
+                mergedArray[mergedArrayIndex] = right[rightIndex];
+                mergedArrayIndex++;
+                rightIndex++;
+                continue;
+            }
+            else if (right[rightIndex] === left[leftIndex]) {
+                mergedArray[mergedArrayIndex] = right[rightIndex];
+                rightIndex++;
+                mergedArrayIndex++;
+
+                mergedArray[mergedArrayIndex] = left[leftIndex];
+                leftIndex++;
+                mergedArrayIndex++;
+
+                continue;
+            }
+        }
+        return mergedArray;
+
+    }
+    else if (arr.length === 1) {
+        return arr;
+    }
+}
 
